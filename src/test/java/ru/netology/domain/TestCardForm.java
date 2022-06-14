34a415fe0ee62f1bdacdfcf409e100bd77fd44fa
DemKosS <sidyakinkn@gmail.com>
2022-06-14 22:39:16 +0300

    Add code

--- src/test/java/ru/netology/domain/TestCardForm.java
+++ src/test/java/ru/netology/domain/TestCardForm.java
@@ -1,6 +1,6 @@
 package ru.netology.domain;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,11 +13,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCardForm {
-    private WebDriver driver;
+    public static WebDriver driver;
 
     @BeforeAll
     public static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "ChromeDriver/chromedriver-2");
     }
 
     @BeforeEach
@@ -28,20 +28,102 @@ public class TestCardForm {
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
+
     @AfterEach
     public void tearDown() {
         driver.quit();
         driver = null;
     }
+
     @Test
     public void shouldSuccessfulCard() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Виктор Иванов");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79206161994");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Алексей Алексеев");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79777123456");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button")).click();
 
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
+
+    @Test   // №2
+    public void shouldEmptyForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    public void shouldEmptyNameField() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79584567788");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    public void shouldNameFieldValidation() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Semen Abramyanc");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79774943579");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    public void shouldNameField() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Анастасия Афиногенова-Криль");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79156667788");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    public void shouldEmptyPhoneField() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Артём Астафьев");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    public void shouldPhoneField() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Арташес Креведов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+7988-722-34-18");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    public void shouldCheck() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Лиза Анастасиади");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+74957665544");
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid .checkbox__text")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных" +
+                " и разрешаю сделать запрос в бюро кредитных историй", text.trim());
+    }
 }
+
